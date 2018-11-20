5304c7afdd57268540aceec875dfc1d5a9d28893
zhouweiwei <zhouweiwei1218@gmail.com>
2018-11-20 17:27:19 +0800

    helloWorld~~~~~~~~~~~~~

--- spring-demo/src/main/java/com/zww/HelloController.java
+++ spring-demo/src/main/java/com/zww/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 	@RequestMapping("/index")
 	public String index(ModelMap map) {
-		map.addAttribute("hello", "hello~~~~~~~~~~~~~");
+		map.addAttribute("hello", "helloWorld~~~~~~~~~~~~~");
 		return "index";
 	}
 }
